e36dd125ce2ec4f19085d2314487bd67c4e2d0cc
David Khourshid <davidkpiano@gmail.com>
2017-04-29 23:25:37 -0400

    Rect method

--- src/rect.js
+++ src/rect.js
@@ -1,5 +1,18 @@
 import Observable from './observable';
 
-export default function rect(node, sampler = Observable.just(null)) {
-  return sampler.map(() => node.getBoundingClientRect());
+export default function rect(node) {
+  if (node instanceof Element) {
+    const nodeRect = node.getBoundingClientRect();
+
+    return {
+      top: nodeRect.top,
+      bottom: nodeRect.bottom,
+      left: nodeRect.left,
+      right: nodeRect.right,
+      height: nodeRect.height,
+      width: nodeRect.width,
+    };
+  } else {
+    throw new Error(node + ' is not an element.');
+  }
 }
